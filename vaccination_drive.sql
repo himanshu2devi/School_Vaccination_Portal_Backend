6baa1338eeff54db882014e6c16ff8988b3d8a5a
Himanshu Devi <himanshu2devi@gmail.com>
2025-05-09 03:03:20 +0530

    Add files via upload

--- vaccination_drive.sql
+++ vaccination_drive.sql
@@ -2,12 +2,14 @@
 -- Query: select * from vaccination_drive
 LIMIT 0, 1000
 
--- Date: 2025-05-05 01:08
+-- Date: 2025-05-09 03:02
 */
-INSERT INTO `` (`id`,`class_name`,`date`,`doses_required`,`vaccine_name`) VALUES (1,'VII','2025-05-10',500,'COVISHIELD');
-INSERT INTO `` (`id`,`class_name`,`date`,`doses_required`,`vaccine_name`) VALUES (2,'XI','2025-05-09',400,'COVAXIN');
-INSERT INTO `` (`id`,`class_name`,`date`,`doses_required`,`vaccine_name`) VALUES (3,'I','2025-05-31',500,'POLIO');
-INSERT INTO `` (`id`,`class_name`,`date`,`doses_required`,`vaccine_name`) VALUES (4,'X','2025-05-12',600,'PFIZER');
-INSERT INTO `` (`id`,`class_name`,`date`,`doses_required`,`vaccine_name`) VALUES (5,'VI','2025-05-07',100,'HPV');
-INSERT INTO `` (`id`,`class_name`,`date`,`doses_required`,`vaccine_name`) VALUES (6,'V','2025-05-06',400,'HEPATITUS A');
-INSERT INTO `` (`id`,`class_name`,`date`,`doses_required`,`vaccine_name`) VALUES (7,'IX','2025-06-06',300,'TYPHOID');
+INSERT INTO `` (`id`,`class_name`,`date`,`doses_required`,`vaccine_name`,`status`) VALUES (13,'III','2025-06-27',401,'POLIO B','APPROVED');
+INSERT INTO `` (`id`,`class_name`,`date`,`doses_required`,`vaccine_name`,`status`) VALUES (14,'X','2025-05-30',100,'COVAXIN A','APPROVED');
+INSERT INTO `` (`id`,`class_name`,`date`,`doses_required`,`vaccine_name`,`status`) VALUES (15,'IX','2025-05-29',200,'COVISHIELD','APPROVED');
+INSERT INTO `` (`id`,`class_name`,`date`,`doses_required`,`vaccine_name`,`status`) VALUES (16,'I','2025-05-27',302,'THPHOID','APPROVED');
+INSERT INTO `` (`id`,`class_name`,`date`,`doses_required`,`vaccine_name`,`status`) VALUES (22,'VIII','2025-05-31',100,'Bacillus Calmette','APPROVED');
+INSERT INTO `` (`id`,`class_name`,`date`,`doses_required`,`vaccine_name`,`status`) VALUES (23,'V','2025-05-27',130,'Varicella-Chickenpox','APPROVED');
+INSERT INTO `` (`id`,`class_name`,`date`,`doses_required`,`vaccine_name`,`status`) VALUES (24,'IX','2025-06-28',150,'Hepatitis B','APPROVED');
+INSERT INTO `` (`id`,`class_name`,`date`,`doses_required`,`vaccine_name`,`status`) VALUES (25,'IX','2025-05-26',70,'ROTAVIRUS','APPROVED');
+INSERT INTO `` (`id`,`class_name`,`date`,`doses_required`,`vaccine_name`,`status`) VALUES (26,'IV','2025-05-25',170,'VARICELLA','PENDING');
